428649bbe270d34099102d399e25f0d5d6334e73
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2022-04-03 13:48:20 -0400

    newslist initial update

---
+++ src/components/NewsList.vue
@@ -0,0 +1,42 @@
+<template>
+	<ul class="news__list">
+		<li v-for="article in articles" class="news__item">{{article.title}}</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	data() {
+	  return {
+	      articles: []
+	  }
+     },
+     created() {
+     	   let self= this;
+           fetch('https://newsapi.org/v2/top-headlines?country=us',
+         {
+
+      headers: {
+          'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+      }
+
+  })
+           .then(function(response){
+             return response.json();
+           })
+           .then(function(data) {
+             console.log(data);
+             self.articles= data.articles;
+           });
+
+        }
+           
+           
+     };
+     
+     
+
+
+
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script>
+import NewsList from '@/components/NewsList.vue'
 export default {
     data() {
         return {
-            message: "Hello World! Welcome to VueJS"
+            message: "Hello World! This is my first Vue Project!" 
+
+            
         };
     },
+    components: { NewsList }
 }
 </script>
 
@@ -13,6 +17,7 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <NewsList></NewsList>
       </div>
     </div>
 </template>
